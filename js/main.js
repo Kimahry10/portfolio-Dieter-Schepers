84a6776f36e903655454bcaa832c7ddb667561b2
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-05-08 14:19:28 +0200

    added about me and skills section mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const hamburger = document.querySelectorAll('.header-image__hamburger');
 const mobileNavigation = document.querySelector('.mobile-navigation');
+const skillsGrid = document.querySelector('.skills-grid');
+
+
+
+
 
 // toggle hamburger
 hamburger.forEach(ham => {
@@ -7,3 +12,10 @@ hamburger.forEach(ham => {
     (mobileNavigation.classList.contains('mobile-navigation--show')) ? mobileNavigation.classList.remove('mobile-navigation--show') : mobileNavigation.classList.add('mobile-navigation--show');
   })
 })
+
+
+// shows all skills
+for (let i = 1; i < 12; i++) {
+  skillsGrid.innerHTML += `<img src='img/skills/skill${i}.svg' >`;
+  
+}
