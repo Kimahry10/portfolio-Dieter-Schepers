487735bc4c835bb30ace475d994e33979171b1b5
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-05-13 22:27:58 +0200

    made desktop landingpage, about me, quote and skills

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const mobileNavigation = document.querySelector('.mobile-navigation');
 const skillsGrid = document.querySelector('.skills-grid');
 const projects = document.querySelector('.projects__list');
 
+const test = document.querySelector('.test');
+
 
 
 
@@ -11,25 +13,44 @@ const projects = document.querySelector('.projects__list');
 hamburger.forEach(ham => {
   ham.addEventListener('click', () => {
     (mobileNavigation.classList.contains('mobile-navigation--show')) ? mobileNavigation.classList.remove('mobile-navigation--show') : mobileNavigation.classList.add('mobile-navigation--show');
-  })
-})
+  });
+});
 
 
 // shows all skills
 for (let i = 1; i < 12; i++) {
   skillsGrid.innerHTML += `<img src='img/skills/skill${i}.svg' >`;
 
-}
+};
+
 
 // loop through json file
-fetch('skills.json')
+let projectIds = []
+fetch('projects.json')
+
   .then(response => response.json())
   .then(data => {
     data.map(project => {
-      const test = project.id;
       projects.innerHTML += `
-      <li><a class='projects__list-item' data-id='${project.id}' href='${project.link}'>${project.title}</a></li>
-      `
-    })
+      <li class='projects__list-item' data-id='${project.id}'>${project.title}</li>`
 
+      projectIds.push(project.id)
+      
+      
+      
+
+      const projectsListItem = document.querySelectorAll('.projects__list-item');
+      projectsListItem.forEach(item => {
+        item.addEventListener('click', () => {
+            test.innerHTML = `
+          ${item.dataset.id}
+          ${project.title}
+          `
+
+        });
+      });
+
+
+    });
   });
+console.log(projectIds)
