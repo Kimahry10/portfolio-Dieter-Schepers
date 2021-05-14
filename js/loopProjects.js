9878f7370e76b50c74f7425e1c7f880742ebd31c
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-05-14 23:19:47 +0200

    linked some links

--- js/loopProjects.js
+++ js/loopProjects.js
@@ -9,6 +9,7 @@ fetch('../projects/projects.json')
         <h3>${project.title}</h3>
         <img src='../${project.img}'>
         <p>${project.text}</p>
+        <a class='projects__list-item__view-project align-bottom' href='${project.link}' target=_blank>view project</a>
       </div>
       `
     })
