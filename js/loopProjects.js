8bffcbba25f122845b8436674f685f25380e156e
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-05-14 23:29:28 +0200

    styled project page buttons

--- js/loopProjects.js
+++ js/loopProjects.js
@@ -6,10 +6,12 @@ fetch('../projects/projects.json')
     data.map(project => {
       allProjectsHTML.innerHTML += `
       <div class='projects-body__projects__project'>
-        <h3>${project.title}</h3>
-        <img src='../${project.img}'>
-        <p>${project.text}</p>
-        <a class='projects__list-item__view-project align-bottom' href='${project.link}' target=_blank>view project</a>
+        <div class='projects-body__projects__project-wrap'>
+          <h3>${project.title}</h3>
+          <img src='../${project.img}'>
+          <p>${project.text}</p>
+        </div>
+        <a class='projects-page__button' href='${project.link}' target=_blank>view project</a>
       </div>
       `
     })
